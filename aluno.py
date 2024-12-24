4420bb08dc0af4e12a2651166a27a8429c99c81e
Fernando Tinno <fernando.venceslau@estudante.ifms.edu.br>
2024-12-23 22:35:02 -0300

    tentativa de linkar o main com o arquivo aluno e retornar ao main(quase dando certo)

--- aluno.py
+++ aluno.py
@@ -1,24 +1,43 @@
 #- Alunos: nome, matrícula (1238734), data de nascimento, sexo, endereço, telefone, e-mail.
 import random
-def cadastro_aluno(nome,data_dia,data_mes,data_ano,sexo,endereco,tel,email,cod_nome):
+import main
+
+def cadastro_aluno():
+    nome = input(f'Qual o nome do aluno(a):')
+    data_dia = input(f'em que dia nasceu o aluno(a):')
+    data_mes = input(f'em que mes nasceu o aluno(a):')
+    data_ano = input(f'em que ano nasceu o aluno(a):')
+    sexo = input(f'sexo do aluno(a):')
+    endereco = input(f'endereço do aluno(a):')
+    tel =  input(f'telefone do aluno(a): ')
+    email = input(f'email do aluno(a):')
+    
+    cod_nome = nome[0].upper()
     matricula = random.randint(1000000,9999999)
     cod_matricula = str(matricula)
     cod_matriculafinal = cod_matricula + cod_nome
-    ficha_aluno = {'nome': nome, 'data de nascimento' : f'{data_dia}/{data_mes}/{data_ano}', 'sexo' : sexo, 'endereco' : endereco, 'telefone' : tel, 'email' : email, 'Numero da matricula' : cod_matriculafinal}
     
-    print(f'cadastro feito com sucesso:')
-    return ficha_aluno
-nome = input(f'Qual o nome do aluno(a):')
-data_dia = input(f'em que dia nasceu o aluno(a):')
-data_mes = input(f'em que mes nasceu o aluno(a):')
-data_ano = input(f'em que ano nasceu o aluno(a):')
-sexo = input(f'sexo do aluno(a):')
-endereco = input(f'endereço do aluno(a):')
-tel =  input(f'telefone do aluno(a): ')
-email = input(f'email do aluno(a):')
-cod_nome = nome[0].upper()
+    lista_aluno = []
+    ficha_aluno = {'nome': nome, 'data de nascimento' : f'{data_dia}/{data_mes}/{data_ano}', 'sexo' : sexo, 'endereco' : endereco, 'telefone' : tel, 'email' : email, 'Numero da matricula' : cod_matriculafinal}
+    nome_aluno = ficha_aluno['nome']
+    lista_aluno.append(nome_aluno)
+    
+    print(f'cadastro feito com sucesso!\n')
+   
+    mostrar = input(f'você gostaria de ver como ficou o cadastro?(digite S ou N)')
+    mostrar_maiusculo = mostrar[0].upper()
+    if mostrar_maiusculo == 'S':
+        print (ficha_aluno)
+        
+    else:
+            escolha=input(f'Ok, voce deseja voltar para o inicio?')
+            escolha_maiusculo = escolha[0].upper()
+    if escolha_maiusculo == 'S':
+                print(f'sefsef')
+    else:
+        print(f'você saiu do GAMPT')
 
-resultado = cadastro_aluno(nome,data_dia,data_mes,data_ano,sexo,endereco,tel,email,cod_nome)
+    
 
-print(resultado)
+
     
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import aluno
-import materia
-import prof
-import turma
+
 
 '''
 - Crie um sistema escolar que permita cadastrar alunos, professores, disciplinas e turmas.
@@ -20,24 +18,26 @@ import turma
 - O sistema deve permitir a consulta de disciplinas alocadas em turmas.
 
 '''
-print(f'\nbem-vindo ao G.A.M.P.T(Gerenciador de Alunos, Matérias, Professores e Turmas)\n')
+def lobby():
+    print(f'\nbem-vindo ao G.A.M.P.T(Gerenciador de Alunos, Matérias, Professores e Turmas)\n')
 
-print(f'quais serviços do GAMPT você deseja acessar?\n')
+    print(f'quais serviços do GAMPT você deseja acessar?\n')
 
-print(f'1-Cadastrar aluno')
-print(f'2-Cadastrar Professor')
-print(f'3-Cadastrar Materia')
-print(f'4-Cadastrar Turma')
-print(f'5-Atribuir aluno a uma turma')
-print(f'6-Atribuir uma disciplina a um professor')
-print(f'7-Atribuir uma disciplina a uma turma')
-print(f'8-Filtrar professor por disciplina')
-print(f'9-Consultar alunos matriculados em turmas especificas')
-print(f'10-Consultar quais são as disciplinas regidas por um professor especifico')
-print(f'11-Consultar quais disciplinas estão interligadas a cada turma')
-print(f'12-Sair\n')
+    print(f'1-Cadastrar aluno')
+    print(f'2-Cadastrar Professor')
+    print(f'3-Cadastrar Materia')
+    print(f'4-Cadastrar Turma')
+    print(f'5-Atribuir aluno a uma turma')
+    print(f'6-Atribuir uma disciplina a um professor')
+    print(f'7-Atribuir uma disciplina a uma turma')
+    print(f'8-Filtrar professor por disciplina')
+    print(f'9-Consultar alunos matriculados em turmas especificas')
+    print(f'10-Consultar quais são as disciplinas regidas por um professor especifico')
+    print(f'11-Consultar quais disciplinas estão interligadas a cada turma')
+    print(f'12-Sair\n')
 
+lobby()
 opcao = input(f'digite o número de acordo com a opção que você deseja:')
 
-
-
+if opcao == '1':
+    aluno.cadastro_aluno()
